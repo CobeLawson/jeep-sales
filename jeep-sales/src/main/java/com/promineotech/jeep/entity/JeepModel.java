ce6a3f7c6abe4f77dfd8d71062914e7b3d80934f
Cobe <cobe.lawson@gmail.com>
2023-04-02 16:51:08 -0500

    first commit

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,12 @@
+package com.promineotech.jeep.entity;
+
+//This Enum lists all the models for our Jeep-sales database
+public enum JeepModel {
+    GRAND_CHEROKEE,
+    CHEROKEE,
+    COMPASS,
+    RENEGADE,
+    WRANGLER,
+    GLADIATOR,
+    WRANGLER_4XE
+}
